309553ee7fea715e0aa1949edccddd63b1eb2ad0
Vishal Balaji <vishalvichu45@gmail.com>
2022-01-20 20:25:11 +0100

    Removing Old files

--- old_files/Detectron2/Train/Train_test.py
+++
@@ -1,7 +0,0 @@
-from detectron2.model_zoo import get_config
-from detectron2.config import LazyConfig
-print(LazyConfig.to_py(get_config('new_baselines/mask_rcnn_R_101_FPN_400ep_LSJ.py')))
-
-# cfg.model.backbone.bottom_up.ResNet.stages.norm = 'BN'
-# cfg.model.backbone.bottom_up.ResNet.stem.norm = 'BN'
-# cfg.model.backbone.norm = 'BN'
--- old_files/YoloV5/custom_train.py
+++
@@ -1,8 +0,0 @@
-"""
-python /home/students-fkk/yolov5/train.py --img 1408 --batch 24 --epochs 150 --data /media/mydisk/videos/training_data/Yolo_data/base.yaml --weights yolov5l6.pt --freeze 10 --workers 12
-
-python train.py --img 1408 --batch 24 --epochs 300 --data /media/mydisk/videos/training_data/Yolo_data/base.yaml --weights yolov5l6.pt --freeze 10 --workers 12
-python yolo_v5_main_files/train.py --img 1408 --batch 32 --epochs 300 --data /media/mydisk/videos/training_data/Yolo_data/base.yaml --weights yolov5s6.pt --freeze 10 --workers 12
-
-python yolo_v5_main_files/train.py --img 1408 --batch 24 --epochs 600 --data /media/mydisk/videos/training_data_new/Yolo_data/base.yaml --weights yolov5l6.pt --freeze 10 --workers 12
-"""
